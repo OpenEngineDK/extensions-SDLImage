84d17ab042d0ee31cdec76d40fa837e0da1e6453
skeen <skeen@cs.au.dk>
2010-11-17 17:19:59 +0000

    Mingw SDLImage fix

--- Setup.cmake
+++ Setup.cmake
@@ -1,5 +1,5 @@
 SET(CMAKE_INCLUDE_PATH ${OE_LIB_DIR}/sdlimage/include/SDL)
-SET(CMAKE_LIBRARY_PATH ${OE_LIB_DIR}/sdlimage/lib/static)
+SET(CMAKE_LIBRARY_PATH ${OE_LIB_DIR}/sdlimage/lib)
 INCLUDE(${CMAKE_ROOT}/Modules/FindSDL_image.cmake)
 INCLUDE(${OE_CURRENT_EXTENSION_DIR}/SetupSDL_image.cmake)
 
